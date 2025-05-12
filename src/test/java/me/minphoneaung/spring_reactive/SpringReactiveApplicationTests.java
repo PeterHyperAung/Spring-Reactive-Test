fa7c721628c544b8f8bfd60b1851fb4ed9491b3b
Min Phone Aung <minphoneaung@startinpoint.com>
2025-05-12 09:24:20 +0630

    set up

---
+++ src/test/java/me/minphoneaung/spring_reactive/SpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minphoneaung.spring_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
